f3bc2cb352f9e6e8a8bd94e5ee96cec597b3267f
chendanet <chenda_net@yahoo.fr>
2021-03-04 23:40:16 +0100

    last one

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,4 +2,31 @@ class EmailsController < ApplicationController
   def index
     @emails = Email.all
   end
+
+  def create
+    @email = Email.create(
+      object: Faker::Games::Pokemon.name,
+      body: Faker::TvShows::HowIMetYourMother.quote)
+
+    respond_to do |format|
+      format.html { redirect_to root_path}
+      format.js {}
+    flash[:notice] = "Email created"
+    end 
+  end
+
+  def show
+    @email = Email.find(params[:id])
+  end
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
 end
